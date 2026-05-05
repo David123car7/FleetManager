3acb7176db33a78fce800dae31bf60a0fc6d9582
CrZ <david123car7@gmail.com>
2026-05-05 14:17:21 +0100

    refactor Result class to use static factory methods

--- include/FleetManager/Common/Models/Result.h
+++ include/FleetManager/Common/Models/Result.h
@@ -9,22 +9,29 @@
 namespace Fleet::Models {
 template <class T = std::nullptr_t> struct Result {
   const std::optional<T> data;
-  const std::string message;
+  const std::optional<std::string> message;
   int httpCode;
 
-  Result(T data, std::string message, int httpCode)
+  Result(std::optional<T> data, std::optional<std::string> message,
+         int httpCode)
       : data{data}, message{message}, httpCode{httpCode} {}
-  Result(std::string message, int httpCode)
-      : data{nullptr}, message{message}, httpCode{httpCode} {}
-  Result(int httpCode) : data{nullptr}, message{""}, httpCode{httpCode} {}
+
+  static Result Sucess(int httpCode = crow::status::OK,
+                       std::optional<T> data = std::nullopt,
+                       std::optional<std::string> message = std::nullopt) {
+    return {data, message, httpCode};
+  }
+
+  static Result Failure(std::string message, int httpCode) {
+    return {std::nullopt, message, httpCode};
+  }
 
   operator crow::json::wvalue() {
     crow::json::wvalue json;
     if (data.has_value())
       json["data"] = data.value();
-    else
-      json["data"] = nullptr;
-    json["message"] = message;
+    if (message.has_value())
+      json["message"] = message.value();
     return json;
   }
 };
--- src/Services/UsersService.cpp
+++ src/Services/UsersService.cpp
@@ -17,26 +17,30 @@ Result<> UsersService::Register(User &user) {
     tx.commit();
   } catch (pqxx::sql_error sql_error) {
     if (sql_error.sqlstate() == "23505")
-      return {UserError::EmailAllreadyExists(), crow::status::CONFLICT};
+      return Result<>::Failure(UserError::EmailAllreadyExists(),
+                               crow::status::CONFLICT);
   } catch (std::exception e) {
     throw e;
   }
-  return {crow::status::OK};
+  return Result<>::Sucess();
 }
 
 Result<std::string> UsersService::Login(LoginRequest req) {
-  pqxx::work tx{*dbConnection};
   try {
+    pqxx::work tx{*dbConnection};
     std::string storedPassword = tx.query_value<std::string>(
         "SELECT password FROM Users WHERE email = $1", pqxx::params{req.email});
     if (storedPassword == req.password) {
-      return {"token", "", crow::status::OK};
+      return Result<std::string>::Sucess(200);
     } else
-      return {"", UserError::WrongPassword(), crow::status::UNAUTHORIZED};
+      return Result<std::string>::Failure(UserError::WrongPassword(),
+                                          crow::status::UNAUTHORIZED);
   } catch (pqxx::unexpected_rows &) {
-    return {"", UserError::UserNotExist(), crow::status::INTERNAL_SERVER_ERROR};
+    return Result<std::string>::Failure(UserError::UserNotExist(),
+                                        crow::status::INTERNAL_SERVER_ERROR);
   } catch (std::exception &e) {
-    return {"", e.what(), crow::status::INTERNAL_SERVER_ERROR};
+    return Result<std::string>::Failure(e.what(),
+                                        crow::status::INTERNAL_SERVER_ERROR);
   }
 }
 } // namespace Fleet::Services
